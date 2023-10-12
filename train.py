6c2ec7941af8bccaae543d38d5a8660e200e468a
Paritosh_Kabra <paritosh_k@cs.iitr.ac.in>
2023-10-12 09:13:24 +0530

    Add gradient clipping and reduce lr

--- train.py
+++ train.py
@@ -26,38 +26,51 @@ parser = argparse.ArgumentParser()
 # Model specific parameters
 parser.add_argument('--input_size', type=int, default=2)
 parser.add_argument('--output_size', type=int, default=5)
-parser.add_argument('--n_epgcn', type=int, default=1, help='Number of EPGCN layers for endpoint prediction')
-parser.add_argument('--n_epcnn', type=int, default=6, help='Number of EPCNN layers for endpoint prediction')
-parser.add_argument('--n_trgcn', type=int, default=1, help='Number of TRGCN layers for trajectory refinement')
-parser.add_argument('--n_trcnn', type=int, default=3, help='Number of TRCNN layers for trajectory refinement')
-parser.add_argument('--n_ways', type=int, default=3, help='Number of control points for endpoint prediction')
-parser.add_argument('--n_smpl', type=int, default=20, help='Number of samples for refine')
+parser.add_argument('--n_epgcn', type=int, default=1,
+                    help='Number of EPGCN layers for endpoint prediction')
+parser.add_argument('--n_epcnn', type=int, default=6,
+                    help='Number of EPCNN layers for endpoint prediction')
+parser.add_argument('--n_trgcn', type=int, default=1,
+                    help='Number of TRGCN layers for trajectory refinement')
+parser.add_argument('--n_trcnn', type=int, default=3,
+                    help='Number of TRCNN layers for trajectory refinement')
+parser.add_argument('--n_ways', type=int, default=3,
+                    help='Number of control points for endpoint prediction')
+parser.add_argument('--n_smpl', type=int, default=20,
+                    help='Number of samples for refine')
 parser.add_argument('--kernel_size', type=int, default=3)
 
 # Data specifc paremeters
 parser.add_argument('--obs_seq_len', type=int, default=8)
 parser.add_argument('--pred_seq_len', type=int, default=12)
-parser.add_argument('--dataset', default='zara1', help='Dataset name(eth,hotel,univ,zara1,zara2)')
+parser.add_argument('--dataset', default='zara1',
+                    help='Dataset name(eth,hotel,univ,zara1,zara2)')
 
 # Training specifc parameters
-parser.add_argument('--batch_size', type=int, default=128, help='Mini batch size')
-parser.add_argument('--num_epochs', type=int, default=512, help='Number of epochs')
-parser.add_argument('--clip_grad', type=float, default=None, help='Gradient clipping')
-parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
-parser.add_argument('--lr_sh_rate', type=int, default=128, help='Number of steps to drop the lr')
-parser.add_argument('--use_lrschd', action="store_true", default=False, help='Use lr rate scheduler')
+parser.add_argument('--batch_size', type=int,
+                    default=128, help='Mini batch size')
+parser.add_argument('--num_epochs', type=int,
+                    default=512, help='Number of epochs')
+parser.add_argument('--clip_grad', type=float,
+                    default=200, help='Gradient clipping')
+parser.add_argument('--lr', type=float, default=0.00005, help='Learning rate')
+parser.add_argument('--lr_sh_rate', type=int, default=128,
+                    help='Number of steps to drop the lr')
+parser.add_argument('--use_lrschd', action="store_true",
+                    default=False, help='Use lr rate scheduler')
 parser.add_argument('--tag', default='tag', help='Personal tag for the model')
 
 args = parser.parse_args()
 
+
 def plot_grad_flow(named_parameters):
     '''Plots the gradients flowing through different layers in the net during training.
     Can be used for checking for possible gradient vanishing / exploding problems.
-    
+
     Usage: Plug this function in Trainer class after loss.backwards() as 
     "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
     ave_grads = []
-    max_grads= []
+    max_grads = []
     layers = []
 
     # for n, p in named_parameters:
@@ -77,8 +90,8 @@ def plot_grad_flow(named_parameters):
     plt.plot(ave_grads, alpha=0.3, color="b")
     # plt.plot(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
     # plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
-    plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k" )
-    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k")
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
     plt.xlim(left=0, right=len(ave_grads))
     # plt.ylim(bottom = -0.001, top=0.02) # zoom in on the lower gradient regions
     plt.xlabel("Layers")
@@ -87,26 +100,32 @@ def plot_grad_flow(named_parameters):
     plt.grid(True)
     plt.savefig(f"img/gradient-hotel")
 
+
 # Data preparation
 # Batch size set to 1 because vertices vary by humans in each scene sequence.
 # Use mini batch working like batch.
 dataset_path = './datasets/' + args.dataset + '/'
 checkpoint_dir = './checkpoint/' + args.tag + '/'
 
-train_dataset = TrajectoryDataset(dataset_path + 'train/', obs_len=args.obs_seq_len, pred_len=args.pred_seq_len, skip=1)
-train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=0, pin_memory=True)
+train_dataset = TrajectoryDataset(
+    dataset_path + 'train/', obs_len=args.obs_seq_len, pred_len=args.pred_seq_len, skip=1)
+train_loader = DataLoader(train_dataset, batch_size=1,
+                          shuffle=True, num_workers=0, pin_memory=True)
 
-val_dataset = TrajectoryDataset(dataset_path + 'val/', obs_len=args.obs_seq_len, pred_len=args.pred_seq_len, skip=1)
-val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
+val_dataset = TrajectoryDataset(
+    dataset_path + 'val/', obs_len=args.obs_seq_len, pred_len=args.pred_seq_len, skip=1)
+val_loader = DataLoader(val_dataset, batch_size=1,
+                        shuffle=False, num_workers=0, pin_memory=True)
 
 # Model preparation
 model = graph_tern(n_epgcn=args.n_epgcn, n_epcnn=args.n_epcnn, n_trgcn=args.n_trgcn, n_trcnn=args.n_trcnn,
                    seq_len=args.obs_seq_len, pred_seq_len=args.pred_seq_len, n_ways=args.n_ways, n_smpl=args.n_smpl)
 model = model.to(device)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=(1e-5)/2)
 if args.use_lrschd:
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_sh_rate, gamma=0.8)
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=args.lr_sh_rate, gamma=0.8)
 
 # Train logging
 if not os.path.exists(checkpoint_dir):
@@ -117,29 +136,31 @@ with open(checkpoint_dir + 'args.pkl', 'wb') as f:
 metrics = {'train_loss': [], 'val_loss': []}
 constant_metrics = {'min_val_epoch': -1, 'min_val_loss': 1e10}
 
+
 def transform_imputed(X):
-    
+
     X_abs = X[:, :, :2]
     X_abs = X_abs.reshape(1, *X_abs.shape)
 
     X_rel = X[:, :, 2:]
     X_rel = X_rel.reshape(1, *X_rel.shape)
 
-    
-    X_abs = X_abs.permute(0,1,3,2)
-    X_rel = X_rel.permute(0,1,3,2)
+    X_abs = X_abs.permute(0, 1, 3, 2)
+    X_rel = X_rel.permute(0, 1, 3, 2)
 
-    S_obs = torch.stack((X_abs, X_rel), dim=1).permute(0,1,4,2,3)
-    
+    S_obs = torch.stack((X_abs, X_rel), dim=1).permute(0, 1, 4, 2, 3)
+
     return S_obs
 
+
 def saits_loader(original_tensor):
-        nelems = original_tensor.numel()
-        ne_nan = int(0.1 * nelems)
-        nan_indices = random.sample(range(nelems), ne_nan)
-        new_tensor = original_tensor.clone().reshape(-1)
-        new_tensor[nan_indices] = float('nan')
-        return new_tensor.reshape(*original_tensor.shape)
+    nelems = original_tensor.numel()
+    ne_nan = int(0.1 * nelems)
+    nan_indices = random.sample(range(nelems), ne_nan)
+    new_tensor = original_tensor.clone().reshape(-1)
+    new_tensor[nan_indices] = float('nan')
+    return new_tensor.reshape(*original_tensor.shape)
+
 
 def train(epoch):
     global metrics, model
@@ -149,7 +170,8 @@ def train(epoch):
     loader_len = len(train_loader)
 
     progressbar = tqdm(range(loader_len))
-    progressbar.set_description('Train Epoch: {0} Loss: {1:.8f}'.format(epoch, 0))
+    progressbar.set_description(
+        'Train Epoch: {0} Loss: {1:.8f}'.format(epoch, 0))
     optimizer.zero_grad()
     for batch_idx, batch in enumerate(train_loader):
         # sum gradients till idx reach to batch_size
@@ -157,7 +179,7 @@ def train(epoch):
             optimizer.zero_grad()
 
         S_obs, S_trgt = [tensor.to(device) for tensor in batch[-2:]]
-        
+
         # Data augmentation
         aug = True
         if aug:
@@ -165,17 +187,18 @@ def train(epoch):
 
         # print(S_obs[:,0].shape)
 
-
-        X_obs_saits = S_obs[:,0].clone().to(device).permute(0, 2, 3, 1)
-        X_obs_rel_saits = S_obs[:, 1].clone().to(device).permute(0,2,3,1)
+        X_obs_saits = S_obs[:, 0].clone().to(device).permute(0, 2, 3, 1)
+        X_obs_rel_saits = S_obs[:, 1].clone().to(device).permute(0, 2, 3, 1)
 
         # print(f'{X_obs_saits.shape=}')
 
         _, npeds, _, step_size = X_obs_saits.shape
-        X_obs_saits = X_obs_saits.permute(0, 1, 3, 2).reshape(npeds, step_size, -1)
+        X_obs_saits = X_obs_saits.permute(
+            0, 1, 3, 2).reshape(npeds, step_size, -1)
 
         _, npeds, _, step_size = X_obs_rel_saits.shape
-        X_obs_rel_saits = X_obs_rel_saits.permute(0, 1, 3, 2).reshape(npeds, step_size, -1)
+        X_obs_rel_saits = X_obs_rel_saits.permute(
+            0, 1, 3, 2).reshape(npeds, step_size, -1)
 
         for i in range(npeds):
             X_i = X_obs_saits[i]
@@ -184,7 +207,6 @@ def train(epoch):
         for i in range(npeds):
             X_i = X_obs_rel_saits[i]
             X_obs_rel_saits[i] = saits_loader(X_i)
-
 
         X_saits = torch.cat((X_obs_saits, X_obs_rel_saits), dim=2)
         X_obs_saits, mae_saits_loss = saits_model(X_saits)
@@ -217,13 +239,15 @@ def train(epoch):
 
         if batch_idx % args.batch_size + 1 == args.batch_size or batch_idx + 1 == loader_len:
             if args.clip_grad is not None:
-                torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad)
+                torch.nn.utils.clip_grad_norm_(
+                    model.parameters(), args.clip_grad)
             optimizer.step()
 
             r_loss_batch = 0.
             m_loss_batch = 0.
 
-        progressbar.set_description('Train Epoch: {0} Loss: {1:.8f}'.format(epoch, loss.item() / args.batch_size))
+        progressbar.set_description('Train Epoch: {0} Loss: {1:.8f}'.format(
+            epoch, loss.item() / args.batch_size))
         progressbar.update(1)
 
     progressbar.close()
@@ -238,7 +262,8 @@ def valid(epoch):
     loader_len = len(val_loader)
 
     progressbar = tqdm(range(loader_len))
-    progressbar.set_description('Valid Epoch: {0} Loss: {1:.8f}'.format(epoch, 0))
+    progressbar.set_description(
+        'Valid Epoch: {0} Loss: {1:.8f}'.format(epoch, 0))
 
     for batch_idx, batch in enumerate(val_loader):
         S_obs, S_trgt = [tensor.to(device) for tensor in batch[-2:]]
@@ -259,7 +284,8 @@ def valid(epoch):
             r_loss_batch = 0.
             m_loss_batch = 0.
 
-        progressbar.set_description('Valid Epoch: {0} Loss: {1:.8f}'.format(epoch, loss.item() / args.batch_size))
+        progressbar.set_description('Valid Epoch: {0} Loss: {1:.8f}'.format(
+            epoch, loss.item() / args.batch_size))
         progressbar.update(1)
 
     progressbar.close()
@@ -269,7 +295,8 @@ def valid(epoch):
     if metrics['val_loss'][-1] < constant_metrics['min_val_loss']:
         constant_metrics['min_val_loss'] = metrics['val_loss'][-1]
         constant_metrics['min_val_epoch'] = epoch
-        torch.save(model.state_dict(), checkpoint_dir + args.dataset + '_best.pth')
+        torch.save(model.state_dict(), checkpoint_dir +
+                   args.dataset + '_best.pth')
 
 
 def main():
@@ -282,8 +309,10 @@ def main():
 
         print(" ")
         print("Dataset: {0}, Epoch: {1}".format(args.tag, epoch))
-        print("Train_loss: {0}, Val_los: {1}".format(metrics['train_loss'][-1], metrics['val_loss'][-1]))
-        print("Min_val_epoch: {0}, Min_val_loss: {1}".format(constant_metrics['min_val_epoch'], constant_metrics['min_val_loss']))
+        print("Train_loss: {0}, Val_los: {1}".format(
+            metrics['train_loss'][-1], metrics['val_loss'][-1]))
+        print("Min_val_epoch: {0}, Min_val_loss: {1}".format(
+            constant_metrics['min_val_epoch'], constant_metrics['min_val_loss']))
         print(" ")
 
         with open(checkpoint_dir + 'metrics.pkl', 'wb') as f:
